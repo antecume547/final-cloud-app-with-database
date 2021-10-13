32b417c981058332bc583ddf1090498e0106d738
antecume547 <79765534+antecume547@users.noreply.github.com>
2021-10-13 22:49:59 +0200

    exam

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -128,7 +128,7 @@ def submit(request, course_id):
                 'subm_id' : subm_id,
                 'submitted_anwsers_ids' : answer_ids,
                 }
-        print (subm.all())
+        print (Submission.objects.all())
         return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', kwargs= datas))
        
 def show_exam_result(request, datas):
