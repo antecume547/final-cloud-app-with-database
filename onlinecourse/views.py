bdc2697a47a3d7cdac9ac22c6f59293b7f8baf99
antecume547 <79765534+antecume547@users.noreply.github.com>
2021-10-14 22:03:05 +0200

    requirement

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -125,7 +125,7 @@ def submit(request, course_id):
             subm.choices.add(id)
             subm.save()
         
-        return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(subm_id,choice_id)))
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(subm_id,course_id)))
        
 def show_exam_result(request,course_id, subm_id):
     context = {'subm_id':subm_id,
